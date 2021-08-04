21e2cb0786cdb9bbfdf32eded1fe1cb11bf0a316
Yusuf YILDIZ <yusuf@jotform.com>
2021-08-04 11:09:07 +0300

    feat(example): create a more clean example page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,124 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import Popup, { PopupProvider } from 'react-native-relative-popup';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import Popup, {
+  PopupProvider,
+  PopupProps,
+  PopupChildrenProps,
+} from 'react-native-relative-popup';
 
-export default function App() {
+type ButtonWithTooltip = {
+  label: string;
+  position: PopupProps['position'];
+};
+
+const ButtonWithTooltip = ({
+  label,
+  position: givenPosition,
+}: ButtonWithTooltip) => {
+  const insets = useSafeAreaInsets();
   const [open, setOpen] = React.useState<boolean>(false);
 
   return (
-    <PopupProvider>
-      <View style={styles.container}>
-        <TouchableOpacity onPress={() => setOpen(true)}>
-          <Text>Button</Text>
+    <TouchableOpacity style={styles.button} onPress={() => setOpen(true)}>
+      <Text style={styles.buttonText}>{label}</Text>
 
-          <Popup isOpen={open} onClose={() => setOpen(false)}>
-            <Text>Popup Content</Text>
-          </Popup>
-        </TouchableOpacity>
-      </View>
-    </PopupProvider>
+      <Popup
+        isOpen={open}
+        position={givenPosition}
+        verticalSpacing={4}
+        safeAreaInsets={insets}
+        onClose={() => setOpen(false)}
+      >
+        {({ position, computedPosition }: PopupChildrenProps) => (
+          <View style={styles.tooltip}>
+            <Text style={styles.tooltipText}>Given position: {position}</Text>
+            <Text style={styles.tooltipText}>
+              Computed position: {computedPosition}
+            </Text>
+          </View>
+        )}
+      </Popup>
+    </TouchableOpacity>
+  );
+};
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <PopupProvider>
+        <SafeAreaView style={styles.app}>
+          <View style={[styles.row, styles.flexStart]}>
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+          </View>
+          <View style={[styles.row, styles.center]}>
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+          </View>
+          <View style={[styles.row, styles.flexEnd]}>
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+            <ButtonWithTooltip position="bottom-center" label="Press Me" />
+          </View>
+        </SafeAreaView>
+      </PopupProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
+    justifyContent: 'space-between',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  flexStart: {
+    alignItems: 'flex-start',
+  },
+  center: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  flexEnd: {
+    alignItems: 'flex-end',
+  },
+  button: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#0052cc',
+    borderRadius: 4,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#fff',
+  },
+  tooltip: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    backgroundColor: '#344563',
+    borderRadius: 4,
+  },
+  tooltipText: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#fff',
   },
 });
